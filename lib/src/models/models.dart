5acf54f737f7737d38016f7932a157e010efd247
Nhat Hung <doraemonidol@gmail.com>
2024-01-31 16:45:25 +0700

    Update models.dart

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -2,3 +2,5 @@ export 'Model/model.dart';
 export 'Challenge/challenge.dart';
 export 'Coupon/coupon.dart';
 export 'TimerActivity/timer_activity.dart';
+export 'Activity/activity.dart';
+export 'User/user.dart';
